f5f6e56a92d3e50bba9218997bedc631c19275c4
Omkommersind <chauzov@list.ru>
2024-04-23 16:32:55 +0300

    Scene load async

--- Assets/Scripts/Inventory/ActionRecievers/ActionSuccessHandlers/GoToScene.cs
+++ Assets/Scripts/Inventory/ActionRecievers/ActionSuccessHandlers/GoToScene.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +8,23 @@ public class GoToScene : MonoBehaviour, IActionSuccessHandler
 
     public void OnSuccess()
     {
-        SceneManager.LoadScene(DestinationScene);
+        //SceneManager.LoadScene(2);
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(DestinationScene);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }
